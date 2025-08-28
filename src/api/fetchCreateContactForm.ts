a92f6715c4fa887e472503af6cd2d100116e9909
Martin <saadv89@gmail.com>
2025-08-28 22:18:52 +0200

    Fix: added correct environment variable to fetch call

--- src/api/fetchCreateContactForm.ts
+++ src/api/fetchCreateContactForm.ts
@@ -1,9 +1,9 @@
 import type { Form } from '../App';
 
 export async function fetchCreateContactForm(form: Form) {
-  console.log(import.meta.env.VITE_API_BASE_URL);
+  console.log(import.meta.env.VITE_API_BASE_URL + 'SaveContact');
 
-  await fetch(`${import.meta.env.BASE_URL}SaveContact`, {
+  await fetch(`${import.meta.env.VITE_API_BASE_URL}SaveContact`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
