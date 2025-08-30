bd576f0169c56a53bc1f289d417be8465556fa5e
Martin <saadv89@gmail.com>
2025-08-30 20:15:38 +0200

    Feat: created a theme selector for light/dark/system themes and store/load the selected theme in/from localstorage

---
+++ src/ThemeSelect.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './ThemeSelect.css';
+
+export type Theme = 'system' | 'light' | 'dark';
+
+export default function ThemeSelect() {
+  const [theme, setTheme] = useState<Theme>((localStorage.getItem('theme') as Theme) || 'system');
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <>
+      <select
+        name=''
+        id=''
+        value={theme}
+        onChange={e => setTheme(e.target.value as Theme)}
+      >
+        <option
+          id='theme-system'
+          value={'system'}
+        >
+          System
+        </option>
+        <option
+          id='theme-light'
+          value={'light'}
+        >
+          Light
+        </option>
+        <option
+          id='theme-dark'
+          value={'dark'}
+        >
+          Dark
+        </option>
+      </select>
+    </>
+  );
+}
