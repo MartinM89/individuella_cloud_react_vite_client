780625e2a2a3d7ff4f21f5f54406c6cd2e8940c0
Martin Michail <saadv89@gmail.com>
2025-09-08 13:05:43 +0200

    - Refactor: wrapped fetch in a try/catch - Refactor: added a api key to access api

--- src/api/fetchCreateContactForm.ts
+++ src/api/fetchCreateContactForm.ts
@@ -1,13 +1,19 @@
 import type { Form } from '../App';
 
-const BASE_API_URL = 'https://ht24individuellacloudmartinm.azurewebsites.net/api/';
+const VITE_API_KEY = import.meta.env.VITE_API_KEY;
+
+const BASE_API_URL = `https://ht24individuellacloudmartinm.azurewebsites.net/api/${VITE_API_KEY}`;
 
 export async function fetchCreateContactForm(form: Form) {
-  await fetch(BASE_API_URL + 'SaveContact', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(form),
-  });
+  try {
+    await fetch(BASE_API_URL + 'SaveContact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    });
+  } catch (error) {
+    console.error('Something went wrong');
+  }
 }
